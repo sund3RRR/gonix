d2ae6cbdb8f859c3d9d77056cf1aa81366338daa
sund3RRR <evenquantity@gmail.com>
2026-06-14 10:10:02 +0300

    feat: init store, add default store dir const

---
+++ const.go
@@ -0,0 +1,8 @@
+package gonix
+
+import "github.com/sund3RRR/gonix/store"
+
+const (
+	// DefaultDir is the default logical Nix store directory, means /nix/store.
+	DefaultStoreDir = store.DefaultDir
+)
---
+++ store/const.go
@@ -0,0 +1,6 @@
+package store
+
+const (
+	// DefaultDir is the default logical Nix store directory, means /nix/store.
+	DefaultDir = "/nix/store"
+)
